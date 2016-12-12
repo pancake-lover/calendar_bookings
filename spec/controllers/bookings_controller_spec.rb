da7b36f00f86d2484827dcad509cec347e9c7ba4
pancake-lover <anankhee@gmail.com>
2016-12-12 14:09:12 +0100

    use FactoryGirl and Rspec

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe BookingsController, type: :controller do
-
+  describe "POST /room_bookings" do
+    it "takes care of empty parameters passed" do
+      room = FactoryGirl.create(:room)
+      post :create, room_id: room.id
+      expect(response.status).to eq(400)
+    end
+  end
 end
