32881f92913e50832408c510a2d83e95893239ff
pancake-lover <anankhee@gmail.com>
2016-12-12 15:07:59 +0100

    add some test cases for bookings controller

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -4,8 +4,44 @@ RSpec.describe BookingsController, type: :controller do
   describe "POST /room_bookings" do
     it "takes care of empty dates passed" do
       room = FactoryGirl.create(:room)
-      post :create, room_id: room.id
+      post :create, params: { room_id: room.id }
       expect(response.status).to eq(422)
+    end
+
+    it "checks if a booking end date is greater than the start date" do
+      room = FactoryGirl.create(:room)
+      post :create, params: { room_id: room.id, start: Date.tomorrow, end: Date.today }
+      expect(response.status).to eq(422)
+    end
+
+    it "books a room if it hasn't any other bookings" do
+      room = FactoryGirl.create(:room)
+      post :create, params: { room_id: room.id, start: Date.today, end: Date.tomorrow }
+      expect(response.status).to eq(200)
+    end
+
+    it "doesn't book a room if there is a booking with the same start date" do
+      booking = FactoryGirl.create(:booking)
+      post :create, params: { room_id: booking.room.id, start: booking.start, end: booking.start+1 }
+      expect(response.status).to eq(422)
+    end
+
+    it "doesn't book a room if there is a booking with the same end date" do
+      booking = FactoryGirl.create(:booking)
+      post :create, params: { room_id: booking.room.id, start: booking.start+1, end: booking.end }
+      expect(response.status).to eq(422)
+    end
+
+    it "doesn't book a room if there is a booking overlapping dates" do
+      booking = FactoryGirl.create(:booking)
+      post :create, params: { room_id: booking.room.id, start: booking.start+1, end: booking.end+1 }
+      expect(response.status).to eq(422)
+    end
+
+    it "books a room if there is no booking with overlapping dates" do
+      booking = FactoryGirl.create(:booking)
+      post :create, params: { room_id: booking.room.id, start: booking.end+1, end: booking.end+2 }
+      expect(response.status).to eq(200)
     end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,10 @@ FactoryGirl.define do
     number 1
     name  "Test room"
   end
+
+  factory :booking do
+    start Date.today
+    add_attribute :end, Date.today+7
+    room FactoryGirl.create(:room)
+  end
 end
