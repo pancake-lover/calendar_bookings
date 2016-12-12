fc8cddad395c7a28317cbdf8a10a08907e15e2b9
pancake-lover <anankhee@gmail.com>
2016-12-12 12:47:03 +0100

    initial refactor of bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,37 +1,15 @@
 class BookingsController < ApplicationController
   def create
-    room = Room.find_by(params[:room_id])
-    bookings_conflicting_with_passed_start_date = room.bookings.where(start: params[:start])
-    bookings_conflicting_with_passed_end_date = room.bookings.where(end: params[:end])
-    if bookings_conflicting_with_passed_start_date.any?
-      render json: { message: 'Booking conflicts with an existing booking' }, status: :unprocessable_entity
-    elsif bookings_conflicting_with_passed_end_date.any?
+    room = Room.find(params[:room_id])
+    start_date = Date.parse(params[:start])
+    end_date = Date.parse(params[:end])
+    conflicting_bookings = room.bookings.where(start: start_date..end_date)
+      .or(room.bookings.where(end: start_date..end_date)).any?
+    if conflicting_bookings
       render json: { message: 'Booking conflicts with an existing booking' }, status: :unprocessable_entity
     else
-      start_date = Date.parse(params[:start])
-      end_date = Date.parse(params[:end])
-      room_vacant = true
-      while(start_date <= end_date) do
-        room.bookings.each do |possibly_conflicting_booking|
-          possibly_conflicting_start_date = possibly_conflicting_booking.start
-          possibly_conflicting_end_date = possibly_conflicting_booking.end
-          while(possibly_conflicting_start_date <= possibly_conflicting_end_date) do
-            if possibly_conflicting_start_date == start_date
-              room_vacant = false
-            end
-            possibly_conflicting_start_date += 1.day
-          end
-        end
-        start_date += 1.day
-      end
-      if room_vacant
-        booking = Booking.new(booking_params)
-        booking.save
-
-        render json: { message: 'Booking created.' }, status: :ok
-      else
-        render json: { message: 'Booking conflicts with an existing booking' }, status: :unprocessable_entity
-      end
+      room.bookings.create(start: start_date, end: end_date)
+      render json: { message: 'Booking created.' }, status: :ok
     end
   end
 
